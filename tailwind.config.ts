9b487ff87611ba35c00314322570aaec92757c94
Mohamad-Mahdi Tasha <imwhdiiii@gmail.com>
2024-02-13 15:49:58 +0330

    added configs of tailwindCSS

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,8 +1,36 @@
+// Codes by mahdi tasha
+// Importing type of tailwindcss
 import type { Config } from "tailwindcss";
 
+// Defining configs of tailwindcss
 const config: Config = {
-  content: [],
-  theme: {},
+  darkMode: 'class',
+  content: [
+    './app/*.{ts,tsx,js,jsx}',
+    './app/**/*.{ts,tsx,js,jsx}',
+    './component/*.{ts,tsx,js,jsx}',
+    './component/*.{ts,tsx,js,jsx}',
+    './chunk/*.{ts,tsx,js,jsx}',
+    './chunk/*.{ts,tsx,js,jsx}',
+  ],
+  theme: {
+    extend: {
+      colors: {
+        primary: '#3498db',
+        secondary: '#2ecc71',
+
+        lightBG: '#F8F9FA',
+        lightText: '#333333',
+        lightBorder: '#dcdcdc',
+
+        darkBG: '#2C3E50',
+        darkText: '#ecf0f1',
+        darkBorder: '#34495e'
+      }
+    }
+  },
   plugins: [],
 };
+
+// Exporting the configs
 export default config;
