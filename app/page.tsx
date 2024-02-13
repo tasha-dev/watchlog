140d9704c7ea6432ed6e1b650376168785d8bdb4
Mohamad-Mahdi Tasha <imwhdiiii@gmail.com>
2024-02-13 16:16:41 +0330

    added home page file

---
+++ app/page.tsx
@@ -0,0 +1,13 @@
+// Codes by mahdi tasha
+// Importing part
+import { ReactNode } from "react";
+
+// Creating and exporting the home page as default
+export default function HomePage():ReactNode {
+  // Returning JSX
+  return (
+    <div>
+      Home Page
+    </div>
+  );
+}
