af676b75ac3f1161169afee9862a8653184a1989
Mohamad-Mahdi Tasha <imwhdiiii@gmail.com>
2024-02-14 15:51:52 +0330

    addedd file of home first section (logged in)

---
+++ component/page/home/logged/firstSectionComponent.tsx
@@ -0,0 +1,13 @@
+// Codes by mahdi tasha
+// Importing part
+import { ReactNode } from "react";
+
+// Creating and exporting first section of home page if user is logged in , as default
+export default function FirstSectionComponent():ReactNode {
+  // Returning JSX
+  return (
+    <div>
+      First Section
+    </div>
+  );
+}
